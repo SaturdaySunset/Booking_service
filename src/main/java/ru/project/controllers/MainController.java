02cee5fefe6a7c6bf7a013984711a9c9024c44a8
Subbotin Danila <soubbota@yandex.ru>
2024-04-18 22:49:13 +0300

    Initial commit

---
+++ src/main/java/ru/project/controllers/MainController.java
@@ -0,0 +1,15 @@
+package ru.project.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "пользователь");
+        return "home";
+    }
+}
